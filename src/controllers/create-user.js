5e87e15bb5ef033a0066ab18798aedf345ab2525
Waldston <waldston.ws@gmail.com>
2024-09-27 06:01:48 -0300

    feat: add CreateUser Controller and validations

---
+++ src/controllers/create-user.js
@@ -0,0 +1,45 @@
+import { CreateUserUseCase } from '../use-cases/create-user.js';
+import validator from 'validator';
+import { badRequest, created, serverError } from './helpers.js';
+
+export class CreateUserController {
+  async execute(httpRequest) {
+    try {
+      const params = httpRequest.body;
+
+      // validar a requisição (Campos obrigatórios e tamanho de senha)
+      const requiredFields = ['first_name', 'last_name', 'email', 'password'];
+
+      for (const field of requiredFields) {
+        if (!params[field] || params[field].trim().lenght === 0) {
+          return badRequest({ message: `Missing param: ${field}` });
+        }
+      }
+
+      const passwordIsValid = params.password.length < 6;
+
+      if (passwordIsValid) {
+        return badRequest({ message: 'Password must be at last 6 characters' });
+      }
+
+      const emailIsValid = validator.isEmail(params.email);
+      if (!emailIsValid) {
+        return badRequest({
+          message: 'Invalid e-mail. Please provide a valid one',
+        });
+      }
+
+      // chamar o use case
+      const createUserUseCase = new CreateUserUseCase();
+
+      const createdUser = await createUserUseCase.execute(params);
+
+      // Retornar resposta para o usuário (status code)
+      return created(createdUser);
+    } catch (error) {
+      console.log(error);
+
+      return serverError();
+    }
+  }
+}
