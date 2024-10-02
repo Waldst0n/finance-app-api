afe5d0fd014bfe44cea6cd76582ba3992bafcdef
Waldston <waldston.ws@gmail.com>
2024-10-02 05:50:23 -0300

    feat: add dependency injection to create user route

--- src/controllers/create-user.js
+++ src/controllers/create-user.js
@@ -1,4 +1,3 @@
-import { CreateUserUseCase } from '../use-cases/index.js';
 import { EmailAlreadyInUseError } from '../errors/user.js';
 import {
   checkIfEmailIsValid,
@@ -11,11 +10,13 @@ import {
 } from './helpers/index.js';
 
 export class CreateUserController {
+  constructor(createUserUseCase) {
+    this.createUserUseCase = createUserUseCase;
+  }
   async execute(httpRequest) {
     try {
       const params = httpRequest.body;
 
-      // validar a requisição (Campos obrigatórios e tamanho de senha)
       const requiredFields = ['first_name', 'last_name', 'email', 'password'];
 
       for (const field of requiredFields) {
@@ -36,10 +37,7 @@ export class CreateUserController {
         return EmailAlreadyInUseResponse();
       }
 
-      // chamar o use case
-      const createUserUseCase = new CreateUserUseCase();
-
-      const createdUser = await createUserUseCase.execute(params);
+      const createdUser = await this.createUserUseCase.execute(params);
 
       // Retornar resposta para o usuário (status code)
       return created(createdUser);
--- src/use-cases/create-user.js
+++ src/use-cases/create-user.js
@@ -1,41 +1,32 @@
 import { v4 as uuidv4 } from 'uuid';
 import bcrypt from 'bcrypt';
-import {
-  PostgresCreateUserRepository,
-  PostgresGetUserByEmailRepository,
-} from '../repositories/postgres/index.js';
 import { EmailAlreadyInUseError } from '../errors/user.js';
 
 export class CreateUserUseCase {
+  constructor(postgresCreateUserRepository, postgresGetUserByEmailRepository) {
+    this.postgresCreateUserRepository = postgresCreateUserRepository;
+    this.getUserByEmailRepository = postgresGetUserByEmailRepository;
+  }
   async execute(createUserParams) {
-    const postgresGetUserByEmailRepository =
-      new PostgresGetUserByEmailRepository();
-
-    const userWithProviderEmail =
-      await postgresGetUserByEmailRepository.execute(createUserParams.email);
+    const userWithProviderEmail = await this.getUserByEmailRepository.execute(
+      createUserParams.email
+    );
 
     if (userWithProviderEmail) {
       throw new EmailAlreadyInUseError(createUserParams.email);
     }
 
-    // Gerar Id do user
     const userId = uuidv4();
 
-    // Criptografar a senha
     const hashedPassword = await bcrypt.hash(createUserParams.password, 10);
 
-    // inserir o usuário
     const user = {
       ...createUserParams,
       id: userId,
       password: hashedPassword,
     };
 
-    //chamar o repositório
-
-    const postgresCreateUserRepository = new PostgresCreateUserRepository();
-
-    const createUser = await postgresCreateUserRepository.execute(user);
+    const createUser = await this.postgresCreateUserRepository.execute(user);
 
     return createUser;
   }
